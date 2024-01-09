79a6d848a7961e550c030513a9ee1794a510925b
Mokhutso <manaswemahlako@gmail.com>
2024-01-09 16:14:03 +0200

    first draft done

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing", {
+    strings: ["Software Engineer","Web Developer", "Cloud Engineer", "CyberSecurity Trainee"], 
+    typeSpeed:60,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+})
