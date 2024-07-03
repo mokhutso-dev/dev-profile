fc8c43b194b0ac28db27bfeec3755e5aa4233ac3
Mokhutso <manaswemahlako@gmail.com>
2024-07-03 17:12:37 +0200

    added proffession title

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing", {
-    strings: ["Software Engineer"], 
+    strings: ["Software Engineer", "DevOps Engineer"], 
     typeSpeed:60,
     backSpeed:100,
     backDelay:1000,
